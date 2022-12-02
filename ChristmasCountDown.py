09a2e7113867688be64d91bf32ab91cd836edd15
Anesu <106879570+Anesu-R@users.noreply.github.com>
2022-12-02 22:56:12 +0000

    Added an if statement
    T: See if there is a way to remove Christmas Countdown for 25 December

--- ChristmasCountDown.py
+++ ChristmasCountDown.py
@@ -1,6 +1,6 @@
 #***Christmas Countdown X Financial Advent Calendar***
 
-#To-do list: 
+#To-do list:
 #(1) Annotate code :)
 #(2) Christmas countdown :)
 #(3) Create an advent calendar of financial knowledge you've learnt this month :
@@ -8,11 +8,14 @@
 
 import datetime
 
-#Setting the variables 
+#Setting the variables
 dt = datetime.datetime
 dtn = dt.now()
 
 #Difference between Christmas and today's date
 daysUntilChristmas = dt(year=2022, month=12, day=25) - dt(year=dtn.year, month=dtn.month, day=dtn.day)
 
-print("Christmas Countdown...\n Days until Christmas: \n {}".format(daysUntilChristmas.days))
+if dt(year=dtn.year, month=dtn.month, day=dtn.day) == dt(year=2022, month=12, day=25):
+    print("It's Christmassss!")
+
+print("Christmas Countdown...\n>>>{}<<< days until Christmas!".format(daysUntilChristmas.days))
