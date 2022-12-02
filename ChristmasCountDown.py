03bfdff24912e188f9ba3e3cd5eaee66dcecdec5
Anesu <106879570+Anesu-R@users.noreply.github.com>
2022-12-02 22:42:15 +0000

    Updating notes to track progress

--- ChristmasCountDown.py
+++ ChristmasCountDown.py
@@ -1,10 +1,18 @@
-#(1) Annotate code, (2) Christmas countdown (3) Create an advent calendar of financial knowledge you learnt (4) random number generator to select day and recap
+#***Christmas Countdown X Financial Advent Calendar***
+
+#To-do list: 
+#(1) Annotate code :)
+#(2) Christmas countdown :)
+#(3) Create an advent calendar of financial knowledge you've learnt this month :
+#(4) random number generator to select a day and recap knowledge :
 
 import datetime
 
+#Setting the variables 
 dt = datetime.datetime
 dtn = dt.now()
 
+#Difference between Christmas and today's date
 daysUntilChristmas = dt(year=2022, month=12, day=25) - dt(year=dtn.year, month=dtn.month, day=dtn.day)
 
-print("Christmas Countdown...\n{} days until Christmas!".format(daysUntilChristmas.days))
+print("Christmas Countdown...\n Days until Christmas: \n {}".format(daysUntilChristmas.days))
