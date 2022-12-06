09cec7965a74aa12648dd51d6b484e2700e4cdf9
Anesu <106879570+Anesu-R@users.noreply.github.com>
2022-12-06 18:00:05 +0000

    Day 2: coding festive images using Python basics

---
+++ 2.FestiveArt.py
@@ -0,0 +1,58 @@
+
+#Festive Image 1
+print("\nMerry Christmas**聖誕快樂**शुभ क्रिसमस**Feliz Navidad**God Jul**")
+
+print("                         *")
+print("                        /_\\")
+print("                       /_ _\\")
+print("                     J_______O")
+print("                     /_______\\")
+print("                   x/_________\\x")
+print("                    /_________\\")
+print("                       |___|\n")
+
+#Festive Image 2
+print("\nHeri Ya Krismasi**メリー クリスマス**Frohe Weihnachten**عيد ميلاد مجيد**Selamat Natal")
+
+print("          ___")
+print("      ___|   |___")
+print("     |___________|")
+print("        1111111")
+print("       111111111")
+print("       111111111")
+print("         11111")
+print("     1111111111111")
+print("   11111111001111111")
+print("   111111111111111111")
+print("    1111111001111111")
+print("     11111111111111")
+print("   111111111111111111")
+print(" 1111111111111111111111")
+print("111111111111111111111111")
+print("111111111111111111111111")
+print("111111111111111111111111")
+print(" 1111111111111111111111")
+print("   111111111111111111")
+print("     11111111111111")
+
+#Festive Image 3
+print("**Maligayang Pasko**Feliz Natal**메리 크리스마스**Uhkisimusi Omuhle")
+print("                                           |\n"*8,"                                         \ /")
+
+print("                                   .    \\ ","|"," /   .")
+print("                                      .  \\ |","/  .")
+print("                                        ,.\\|/.,")
+print("                                ","="*21)
+print("                                        './|\\.'")
+print("                                      .  /","|","\\  .")
+print("                                    .   /  |"," \\   .")
+print("                                           |")
+print("                                           |")
+print("**Joyeux Noël**Wesołych Świąt**Χαρούμενα Χριστούγεννα**Ayyaana dhalootaa gaarii**")
+
+
+
+
+
+
+
